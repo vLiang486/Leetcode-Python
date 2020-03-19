1edccb8ae852f35fec45d87fa693b67cf53db24c
vLiang486 <vliang486@gmail.com>
2020-03-19 16:41:17 +0800

    Create day3_merge-two-sorted-lists.py

---
+++ day3_merge-two-sorted-lists.py
@@ -0,0 +1,30 @@
+'''
+21. 合并两个有序链表
+https://leetcode-cn.com/problems/merge-two-sorted-lists/
+将两个有序链表合并为一个新的有序链表并返回。新链表是通过拼接给定的两个链表的所有节点组成的。 
+解题思路:
+通过递归方法判断2个链表的大小是否相同进行一一对比拼接。
+'''
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def mergeTwoLists(self, l1: ListNode, l2: ListNode) -> ListNode:
+        if l1 is None:
+            return l2
+        elif l2 is None:
+            return l1
+        elif l1.val < l2.val:
+            l1.next = self.mergeTwoLists(l1.next, l2)
+            return l1
+        elif l1.val >= l2.val:
+            l2.next = self.mergeTwoLists(l1,l2.next)
+            return l2
+
+        
+        
+            
